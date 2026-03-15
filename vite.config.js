bfea4b99186b1ed19a56441facec9da79e29f4cf
Vesselgugugu-AFK <asamugugugu@gmail.com>
2026-03-15 14:22:48 +0800

    update base path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  // 核心修改：添加 base 路径，对应你的 GitHub 仓库名
+  base: '/vesophone/', 
   plugins: [vue()],
   resolve: {
     alias: {
